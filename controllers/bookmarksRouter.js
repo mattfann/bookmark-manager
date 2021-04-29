54034b467ae7c1a209e36e3c0b887d10dc2562ad
ndowkunda <mn057@outlook.com>
2021-04-29 16:41:48 +0100

    adding update functionality

--- controllers/bookmarksRouter.js
+++ controllers/bookmarksRouter.js
@@ -1,48 +1,58 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bookmarksRouter = express.Router();
 
-const { models } = require('../models');
+const { models } = require("../models");
 
-bookmarksRouter.get('/', async (req, res) => {
-    const bookmarks = await models.Bookmark.findAll();
-    const array = [];
-    for(let i = 0; i < bookmarks.length; i++) {
-        let url = bookmarks[i].dataValues.url;
-        let comment = bookmarks[i].dataValues.comment;
-        let tag = bookmarks[i].dataValues.tags;
-        let id = bookmarks[i].dataValues.id;
+bookmarksRouter.get("/", async (req, res) => {
+	const bookmarks = await models.Bookmark.findAll();
+	const array = [];
+	for (let i = 0; i < bookmarks.length; i++) {
+		let url = bookmarks[i].dataValues.url;
+		let comment = bookmarks[i].dataValues.comment;
+		let tag = bookmarks[i].dataValues.tags;
+		let id = bookmarks[i].dataValues.id;
 
-        array.push([url,comment,tag, id]);
-        
-    };
-    res.render('pages/index', {
-        bookmarkarray: array,
-    }); 
+		array.push([url, comment, tag, id]);
+	}
+	res.render("pages/index", {
+		bookmarkarray: array,
+	});
 });
 
-bookmarksRouter.post('/add', async (req, res) => {
-    let url = req.body.url
-    let comment = req.body.comment
-    let tags = req.body.tags
-    await models.Bookmark.create({url:url, comment:comment, tags:tags})
-    res.redirect('/'); 
-})
+bookmarksRouter.post("/add", async (req, res) => {
+	let url = req.body.url;
+	let comment = req.body.comment;
+	let tags = req.body.tags;
+	await models.Bookmark.create({ url: url, comment: comment, tags: tags });
+	res.redirect("/");
+});
 
-bookmarksRouter.delete('/:bookmarkId', async (req, res) => {
-    await models.Bookmark.destroy({
-        where: {
-           id:  req.params.bookmarkId 
-        }
-    })
+bookmarksRouter.delete("/:bookmarkId", async (req, res) => {
+	await models.Bookmark.destroy({
+		where: {
+			id: req.params.bookmarkId,
+		},
+	});
 
-   res.redirect('/'); 
-})
+	res.redirect("/");
+});
 
+bookmarksRouter.put("/:bookmarkId", async (req, res) => {
+	console.log(req.body.updatedUrl);
+	await models.Bookmark.update(
+		{
+			url: req.body.updatedUrl,
+			comment: req.body.updatedComment,
+			tags: req.body.updatedTags,
+		},
+		{
+			where: {
+				id: req.params.bookmarkId,
+			},
+		}
+	);
+	res.redirect("/");
+});
 
-
-
-
-
-
-module.exports = bookmarksRouter
+module.exports = bookmarksRouter;
